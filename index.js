69c646aaa9b79d0edb997fbe8d7020601fb60e6f
Robert <robertschauss91@gmail.com>
2023-01-14 22:45:41 -0800

    everything is all set up

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generate = require("./generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+console.log(process.argv);
